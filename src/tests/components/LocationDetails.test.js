bf74e841f5ffd8b83d10a60916a941b98be121b0
HJ Kang <hkang07@gmail.com>
2023-01-18 23:34:13 +0000

    first test on location details

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -2,3 +2,12 @@ import React from "react";
 import { render } from "@testing-library/react";
 import LocationDetails from "../../components/LocationDetails";
 
+describe("LocationDetails", () => {
+  it("it renders correct city and location props", () => {
+    const { getByText } = render(
+      <LocationDetails city="Manchester" country="UK" />
+    );
+
+    expect(getByText("Manchester, UK")).toBeInstanceOf(HTMLHeadingElement);
+  });
+});
